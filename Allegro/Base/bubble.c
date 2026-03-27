e034de65b707e92991ee2b0b0fd487a472e76699
jeongwon-in <dnwnrudckf123@gmail.com>
2026-03-27 10:04:49 +0900

    a

--- Allegro/Base/bubble.c
+++ Allegro/Base/bubble.c
@@ -6,30 +6,31 @@
 #define BUBBLES_N 128
 stBUBBLE bubbles[BUBBLES_N];
 
-#define SHIP_H 1
-#define SHOTS_N 1
-#define SHIP_SHOT_H 1
-#define SHIP_SHOT_W 1
+#define PLAYER_H 10
+#define SHOTS_N 10
+#define BUBBLE_SHOT_H 10
+#define BUBBLE_SHOT_W 10
 
 
 
-void bubble_init()
+void bubble_init(stBUBBLE* bubbles)
 {
     for (int i = 0; i < BUBBLES_N; i++)
         bubbles[i].used = false;
 }
 
 // bubble
-bool bubble_add(stPLAYER player)
+bool bubble_add(stPLAYER* player, stBUBBLE* bubbles)
 {
 
     for (int i = 0; i < SHOTS_N; i++)
     {
         if (bubbles[i].used) continue;
 
-        bubbles[i].x = player.obj.phy.pos.x;
-        bubbles[i].y = player.obj.phy.pos.y + (SHIP_H / 2) - (SHIP_SHOT_H / 2); // center pos
-
+        bubbles[i].x = player->obj.phy.pos.x;
+        bubbles[i].y = player->obj.phy.pos.y + (PLAYER_H / 2) - (BUBBLE_SHOT_H / 2); // center pos
+        bubbles[i].dx = 0;
+        bubbles[i].dy = 0;
 
         bubbles[i].frame = 0;   // 0 frame used
         bubbles[i].used = true; // buble used
@@ -40,53 +41,56 @@ bool bubble_add(stPLAYER player)
     return false;
 }
 
-void bubble_update()
+void bubble_update(stPLAYER* player, stBUBBLE* bubbles)
 {
     for (int i = 0; i < BUBBLES_N; i++)
     {
         if (!bubbles[i].used)
             continue;
 
-        // look, left: x-=, right: x+=
-        // bubbles[i].y -= 5;
-        if (bubbles[i].y < -SHIP_SHOT_H) // 캐릭터 크기 받기
-        {
-            bubbles[i].used = false;
-            continue;
+        if (bubbles[i].frame >= bubbles[i].dur) {
+
+        }
+
+        if (player->obj.phy.look == eDIR_LOOK_RIGHT) {
+            bubbles[i].dx += 5;
+        }
+        else if (player->obj.phy.look == eDIR_LOOK_LEFT) {
+            bubbles[i].dx -= 5;
         }
 
         bubbles[i].frame++; // 프레임 증가
     }
 }
 
-bool bubble_collide(bool ship, int x, int y, int w, int h)
-{
-    for (int i = 0; i < BUBBLES_N; i++)
-    {
-        if (!bubbles[i].used)
-            continue;
-
-        int sw, sh;
-        if (ship)
-        {
-            //sw = ALIEN_SHOT_W;
-            //sh = ALIEN_SHOT_H;
-        }
-        else
-        {
-            sw = SHIP_SHOT_W;
-            sh = SHIP_SHOT_H;
-        }
-
-        //if (collide(x, y, x + w, y + h, bubbles[i].x, bubbles[i].y, bubbles[i].x + sw, bubbles[i].y + sh))
-        //{
-        //    bubbles[i].used = false;
-        //    return true;
-        //}
-    }
-
-    return false;
-}
+//bool bubble_collide(bool ship, int x, int y, int w, int h)
+//{
+//    for (int i = 0; i < BUBBLES_N; i++)
+//    {
+//        if (!bubbles[i].used)
+//            continue;
+//
+//        int sw, sh;
+//        if (ship)
+//        {
+//            //sw = ALIEN_SHOT_W;
+//            //sh = ALIEN_SHOT_H;
+//        }
+//        else
+//        {
+//            sw = BUBBLE_SHOT_W;
+//            sh = BUBBLE_SHOT_H;
+//        }
+//
+//        //if (collide(x, y, x + w, y + h, bubbles[i].x, bubbles[i].y, bubbles[i].x + sw, bubbles[i].y + sh))
+//        //{
+//        //    bubbles[i].used = false;
+//        //    return true;
+//        //}
+//    }
+//
+//    return false;
+//}
 
 
 
